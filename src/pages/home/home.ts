d17be08e358dcc823413de0577430a286ea3051f
valexx55 <valexx55@gmail.com>
2018-06-19 13:56:11 +0200

    prov1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BusquedaObservableService, BusquedaInterface, SearchItemReducido, SearchItem } from 'itunes-ionic';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [{provide: 'BusquedaInterface', useClass: BusquedaObservableService}]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private termino:string;
+  private resultados : SearchItem[];
+
+  constructor(public navCtrl: NavController, @Inject('BusquedaInterface') private itunes_service:BusquedaInterface) {
 
   }
+
+  buscar(evento)
+  {
+    console.log ("evento" + evento);
+    console.log ("termino" + this.termino);
+    this.itunes_service.busca(this.termino).subscribe (
+      ok => {
+        console.log (ok);
+        this.resultados = <SearchItem[]>ok.results;
+        console.log (this.resultados);
+          }
+    )
+  }
+
+
 
 }
